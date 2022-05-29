ead81537c43703578ea90c023e6483503803dddb
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-05-29 20:15:20 +0200

    slides url, sources and href changed

--- js/index.js
+++ js/index.js
@@ -14,11 +14,11 @@ function init() {
 
   var width = 200;
   var height = 100;
-
+  var link = window.location.href;
   var slide = new Slide(width, height, 'out');
 	var l1 = new THREE.ImageLoader();
 	l1.setCrossOrigin('Anonymous');
-	l1.load('http://localhost/lamolienda/img/bg-1.png', function(img) {
+	l1.load(link+'img/bg-1.png', function(img) {
 	  slide.setImage(img);
 	})
   root.scene.add(slide);
@@ -26,7 +26,7 @@ function init() {
   var slide2 = new Slide(width, height, 'in');
   var l2 = new THREE.ImageLoader();
 	l2.setCrossOrigin('Anonymous');
-	l2.load('http://localhost/lamolienda/img/bg-2.png', function(img) {
+	l2.load(link+'img/bg-2.png', function(img) {
 		slide2.setImage(img);
 	})
 	
